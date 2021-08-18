34d9740dc78a474fbb04750848c3f23f5dfbdaef
ruifernandees <dev.ruifernandes@gmail.com>
2021-08-18 14:42:16 -0300

    feat: ConvertRawLecturesUseCase

---
+++ src/useCases/ConvertRawLectures/ConvertRawLecturesUseCase.js
@@ -0,0 +1,33 @@
+import { Lecture } from "../../entities/Lecture";
+
+export class ConvertRawLecturesUseCase
+{
+    constructor() {}
+
+    execute(rawLectures)
+    {
+        const lectures = [];
+
+        for (const rawLecture of rawLectures) {
+            const stringLightningMinutes = rawLecture.replace(/lightning/, "5min");
+            let minutesString = stringLightningMinutes.match(/\d+min/);
+
+            if (minutesString) {
+                minutesString = minutesString[0];
+                let minutes = minutesString.match(/\d+/);
+
+                if (minutes) {
+                    minutes = Number(minutes[0]);
+
+                    const lectureTitle = stringLightningMinutes.split(minutesString)[0].trim();
+
+                    const lecture = new Lecture(lectureTitle, minutes, rawLecture);
+                    lectures.push(lecture);
+                }
+            }
+
+        }
+
+        return lectures;
+    }
+}
