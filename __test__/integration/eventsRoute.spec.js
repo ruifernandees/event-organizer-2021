f5e4a7df4626c28d4966727bfaa120e18694358d
ruifernandees <dev.ruifernandes@gmail.com>
2021-08-20 20:04:59 -0300

    feat(eventsRoute): add event tests

--- __test__/integration/eventsRoute.spec.js
+++ __test__/integration/eventsRoute.spec.js
@@ -1,8 +1,26 @@
 import request from 'supertest';
 
 import app from '../../src/app';
+import { event } from '../mocks/event';
+import duplicatedLecturesEvent from '../mocks/duplicatedLecturesEvent.json';
 
 describe('File upload', () => {
+    it('Should return event correctly', async () => {
+        const response = await request(app)
+            .post('/events')
+            .attach('file', '__test__/files/proposals.txt');
+
+        expect(JSON.stringify(response.body)).toBe(JSON.stringify(event));
+    });
+
+    it('Should return event with duplicated lectures correctly', async () => {
+        const response = await request(app)
+            .post('/events')
+            .attach('file', '__test__/files/proposalsDuplicated.txt');
+        
+        expect(JSON.stringify(response.body)).toBe(JSON.stringify(duplicatedLecturesEvent));
+    });
+
     it('Should return status 200 when upload a text/plain file', async () => {
         const response = await request(app)
             .post('/events')
