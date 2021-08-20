475d2cdfab639b4d1487dca5960c7ee35a5d23ba
ruifernandees <dev.ruifernandes@gmail.com>
2021-08-20 16:42:08 -0300

    feat(OrganizeEventUseCase): test double lectures

--- src/useCases/OrganizeEventUseCase/__tests__/OrganizeEventUseCase.spec.js
+++ src/useCases/OrganizeEventUseCase/__tests__/OrganizeEventUseCase.spec.js
@@ -1,7 +1,9 @@
+import { Lecture } from "../../../entities/Lecture";
 import { OrganizeEventUseCase } from "../OrganizeEventUseCase";
 import { event } from "./mocks/event";
 import { lectures } from './mocks/lectures';
 import { lecturesShuffled } from "./mocks/lecturesShuffled";
+import duplicatedLectures from './mocks/duplicatedLectures.json';
 
 describe("Organize Event Use Case", () => {
     it ("Should return right result", () => {
@@ -39,5 +41,23 @@ describe("Organize Event Use Case", () => {
         }
 
         expect(times).toEqual([180, 240, 180, 185]);
+    });
+
+    it ("Should return correct event with duplicated lectures", () => {
+        const organizeEventUseCase = new OrganizeEventUseCase();
+
+        const newLectures = lectures.map((lecture) => {
+            const newLecture = new Lecture(
+                lecture.title + " v2",
+                lecture.timeInMinutes,
+                lecture.rawTitle,
+                lecture.hour
+            );
+            return newLecture;
+        });
+
+        const result = organizeEventUseCase.execute(lectures.concat(newLectures));
+
+        expect(JSON.stringify(result)).toBe(JSON.stringify(duplicatedLectures));
     });
 });
