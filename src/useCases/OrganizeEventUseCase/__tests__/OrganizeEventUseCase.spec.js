937e5b716b117f6faab3513d4f4015ca55f509ca
ruifernandees <dev.ruifernandes@gmail.com>
2021-08-20 15:42:17 -0300

    feat(OrganizeEventeUseCase tests): time tests

--- src/useCases/OrganizeEventUseCase/__tests__/OrganizeEventUseCase.spec.js
+++ src/useCases/OrganizeEventUseCase/__tests__/OrganizeEventUseCase.spec.js
@@ -1,6 +1,7 @@
 import { OrganizeEventUseCase } from "../OrganizeEventUseCase";
 import { event } from "./mocks/event";
 import { lectures } from './mocks/lectures';
+import { lecturesShuffled } from "./mocks/lecturesShuffled";
 
 describe("Organize Event Use Case", () => {
     it ("Should return right result", () => {
@@ -8,5 +9,35 @@ describe("Organize Event Use Case", () => {
         const result = organizeEventUseCase.execute(lectures);
 
         expect(result).toEqual(event);
+    });
+    
+    it ("Should have the right time", () => {
+        const organizeEventUseCase = new OrganizeEventUseCase();
+        const result = organizeEventUseCase.execute(lectures);
+        
+        const times = [];
+
+        for (let track of result)
+        {
+            times.push(track.morning.timeOfLectures);
+            times.push(track.afternoon.timeOfLectures);
+        }
+
+        expect(times).toEqual([180, 240, 180, 185]);
+    });
+
+    it ("Should have the same time", () => {
+        const organizeEventUseCase = new OrganizeEventUseCase();
+        const result = organizeEventUseCase.execute(lecturesShuffled);
+        
+        const times = [];
+
+        for (let track of result)
+        {
+            times.push(track.morning.timeOfLectures);
+            times.push(track.afternoon.timeOfLectures);
+        }
+
+        expect(times).toEqual([180, 240, 180, 185]);
     });
 });
